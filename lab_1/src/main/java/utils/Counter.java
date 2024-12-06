976d8e4c5db51b32fcdddf89fe61b4243cee0454
dmarek03 <domarek@student.agh.edu.pl>
2024-12-06 22:14:54 +0100

    Added lab_1

---
+++ lab_1/src/main/java/utils/Counter.java
@@ -0,0 +1,26 @@
+package utils;
+class Counter {
+    private int _val;
+
+
+    public Counter(int n) {
+        _val = n;
+    }
+    public void inc() {
+
+        _val++;
+
+
+
+    }
+    public void dec() {
+
+        _val--;
+
+
+    }
+    public int value() {
+        return _val;
+    }
+}
+
